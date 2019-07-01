0171ce527ea75c82574ba31eacf71ccc3c56b887
Romain <romain.fairant@orness.com>
2019-07-01 14:16:01 +0000

    add annotations

--- service/workflow-uid-service/src/main/java/com/orness/gandalf/core/service/workflowuidservice/WorkflowUidServiceApplication.java
+++ service/workflow-uid-service/src/main/java/com/orness/gandalf/core/service/workflowuidservice/WorkflowUidServiceApplication.java
@@ -1,10 +1,14 @@
 package com.orness.gandalf.core.service.workflowuidservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@Configuration
+@EnableAutoConfiguration
 @SpringBootApplication
 @EntityScan(basePackages = {"com.orness.gandalf.core.workflowuidmodule.manager"})
 @EnableJpaRepositories(basePackages = {"com.orness.gandalf.core.workflowuidmodule.repository"})
--- test/job-print/src/main/java/com/orness/gandalf/core/test/jobprint/JobTestPrintApplication.java
+++ test/job-print/src/main/java/com/orness/gandalf/core/test/jobprint/JobTestPrintApplication.java
@@ -2,10 +2,14 @@ package com.orness.gandalf.core.test.jobprint;
 
 import io.zeebe.client.ZeebeClient;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableAutoConfiguration
 @SpringBootApplication
 @EnableDiscoveryClient
 public class JobTestPrintApplication {
