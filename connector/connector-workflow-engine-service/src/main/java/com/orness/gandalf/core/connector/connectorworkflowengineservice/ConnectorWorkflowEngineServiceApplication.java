e4c5afd1e0ff1340ae7f4b6da7fc6b646f5089de
Romain <romain.fairant@orness.com>
2019-07-01 14:12:06 +0000

    Add annotations

--- connector/connector-workflow-engine-service/src/main/java/com/orness/gandalf/core/connector/connectorworkflowengineservice/ConnectorWorkflowEngineServiceApplication.java
+++ connector/connector-workflow-engine-service/src/main/java/com/orness/gandalf/core/connector/connectorworkflowengineservice/ConnectorWorkflowEngineServiceApplication.java
@@ -1,9 +1,13 @@
 package com.orness.gandalf.core.connector.connectorworkflowengineservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableAutoConfiguration
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ConnectorWorkflowEngineServiceApplication {
--- test/java-kafka-2/src/main/java/com/orness/gandalf/core/test/javakafka2/JavaKafka2Application.java
+++ test/java-kafka-2/src/main/java/com/orness/gandalf/core/test/javakafka2/JavaKafka2Application.java
@@ -1,10 +1,14 @@
 package com.orness.gandalf.core.test.javakafka2;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableAutoConfiguration
 @SpringBootApplication
 @EnableDiscoveryClient
 @EntityScan(basePackages = {"com.orness.gandalf.core.messagemodule.domain"})
