86d9f4607f3888af26aa1aa98c187569a0e825fb
Romain Fairant <romain.fairant@orness.com>
2019-07-02 16:34:02 +0200

    Diagram demo

--- service/workflow-service/src/main/java/com/orness/gandalf/core/service/workflowservice/sample/WorkflowServiceSample.java
+++ service/workflow-service/src/main/java/com/orness/gandalf/core/service/workflowservice/sample/WorkflowServiceSample.java
@@ -30,7 +30,22 @@ public class WorkflowServiceSample implements CommandLineRunner {
     public void run(String... args) throws Exception {
         System.out.println("Start sample");
         //testPerformanceLoop(100, false);
-        testJob();
+        demoPresentation();
+    }
+
+    public void demoPresentation() {
+        DeploymentEvent deploymentEvent;
+        WorkflowInstanceEvent workflowInstanceEvent;
+        ZeroMQJavaClient zeroMQJavaClient = new ZeroMQJavaClient(connectionWorker, connectionSubscriber);
+        zeroMQJavaClient.subscribeWorkflowEngineTopic("webhook");
+        //WORKFLOW
+        String name = "diagram_1.bpmn";
+        deploymentEvent = workflowManager.deployWorkflow(name);
+        String id = workflowManager.getIdDeployment(deploymentEvent);
+        System.out.println(id);
+        String instance_name_continue = name;
+        String instance_content = "content_" + instance_name_continue;
+        workflowInstanceEvent = workflowManager.InstanceWorkflow(id, instance_name_continue,instance_content, "webhook","");
     }
 
     public void testJob() {
