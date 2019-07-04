dfe8fbf73072ceaf0ef4cbd65557850721bcf032
Romain Fairant <romain.fairant@orness.com>
2019-07-04 18:37:40 +0200

    blipouille

--- job/build-job/src/main/java/com/orness/gandalf/core/job/buildjob/feign/BuildFeign.java
+++ job/build-job/src/main/java/com/orness/gandalf/core/job/buildjob/feign/BuildFeign.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value = "build-service.service.gandalf")
+@FeignClient(value = "build-service")
 public interface BuildFeign {
 
     @RequestMapping(method = RequestMethod.POST, value = "/build")
