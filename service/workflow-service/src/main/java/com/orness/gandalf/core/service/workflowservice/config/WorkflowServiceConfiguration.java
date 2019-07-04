c1c281d889342f7f6654ec802a6533e19c2cf8c8
Romain Fairant <romain.fairant@orness.com>
2019-07-04 11:50:20 +0200

    add config workflow-service

---
+++ service/workflow-service/src/main/java/com/orness/gandalf/core/service/workflowservice/config/WorkflowServiceConfiguration.java
@@ -0,0 +1,22 @@
+package com.orness.gandalf.core.service.workflowservice.config;
+
+import io.zeebe.client.ZeebeClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WorkflowServiceConfiguration {
+
+    @Value("${gandalf.workflow.broker}")
+    private String brokerAddress;
+
+    @Bean
+    public ZeebeClient zeebe() {
+        //Client
+        ZeebeClient zeebeClient = ZeebeClient.newClientBuilder()
+                .brokerContactPoint(brokerAddress)
+                .build();
+        return zeebeClient;
+    }
+}
