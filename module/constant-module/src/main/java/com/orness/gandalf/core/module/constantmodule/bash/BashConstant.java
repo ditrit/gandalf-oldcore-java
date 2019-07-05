f36a6eb53e2d8dbc6be36f9078d44f10ec728b73
Romain Fairant <romain.fairant@orness.com>
2019-07-05 12:50:18 +0200

    ini file

--- module/constant-module/src/main/java/com/orness/gandalf/core/module/constantmodule/bash/BashConstant.java
+++ module/constant-module/src/main/java/com/orness/gandalf/core/module/constantmodule/bash/BashConstant.java
@@ -5,7 +5,6 @@ public class BashConstant {
     public static final String SCRIPT_BUILD_DIRECTORY = System.getProperty("user.dir") + "/builds";
     public static final String SCRIPT_CLONE = "git clone ";
     public static final String SCRIPT_BUILD = "mvn clean install";
-    public static final String SCRIPT_DEPLOY_CONF_FILE = "conf.ini";
     public static final String SCRIPT_DEPLOY_DIRECTORY = System.getProperty("user.dir") + "/builds";
     public static final String SCRIPT_COMMAND_FILE = "manage_svc.sh";
     public static final String SCRIPT_REGISTER_FILE = "register.sh";
