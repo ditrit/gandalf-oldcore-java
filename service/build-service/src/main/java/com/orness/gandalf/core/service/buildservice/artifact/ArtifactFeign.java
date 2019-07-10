3513be4203793183134bf69a1ca7f6df14f25ffe
Romain Fairant <romain.fairant@orness.com>
2019-07-10 10:15:15 +0200

    feign 2

--- service/build-service/src/main/java/com/orness/gandalf/core/service/buildservice/artifact/ArtifactFeign.java
+++ service/build-service/src/main/java/com/orness/gandalf/core/service/buildservice/artifact/ArtifactFeign.java
@@ -1,18 +1,45 @@
 package com.orness.gandalf.core.service.buildservice.artifact;
 
-import feign.Param;
+import feign.codec.Encoder;
+import feign.form.spring.SpringFormEncoder;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.support.SpringEncoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
 
-@FeignClient(name = "artifact-service", url = "artifact-service.service.gandalf:10000", configuration = FeignConfig.class)
+@FeignClient(name = "artifact-service", url = "artifact-service.service.gandalf:10000", configuration = ArtifactFeign.FeignConfig.class)
 public interface ArtifactFeign {
 
-    @RequestMapping(method = RequestMethod.POST, value = "/upload/file")
-    void uploadBuildFile(@Param("file") MultipartFile file);
+    @RequestMapping(method = RequestMethod.POST, value = "/upload/file", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    void uploadBuildFile(@RequestPart("file") MultipartFile file);
 
-    @RequestMapping(method = RequestMethod.POST, value = "/upload/conf")
-    void uploadBuildConf(@Param("conf") MultipartFile conf);
+    @RequestMapping(method = RequestMethod.POST, value = "/upload/conf", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    void uploadBuildConf(@RequestPart("conf") MultipartFile conf);
+
+    @Configuration
+    class FeignConfig {
+
+        @Autowired
+        private ObjectFactory<HttpMessageConverters> messageConverters;
+
+        @Bean
+        @Primary
+        @Scope("prototype")
+        public Encoder feignEncoder() {
+            return new SpringFormEncoder(new SpringEncoder(messageConverters));
+        }
+    }
 }
+
+
