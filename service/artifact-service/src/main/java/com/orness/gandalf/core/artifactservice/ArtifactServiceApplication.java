3fd404111234f7b7e02961979c726b80a53c8990
Romain Fairant <romain.fairant@orness.com>
2019-07-17 11:24:09 +0200

    resolver

--- service/artifact-service/src/main/java/com/orness/gandalf/core/artifactservice/ArtifactServiceApplication.java
+++ service/artifact-service/src/main/java/com/orness/gandalf/core/artifactservice/ArtifactServiceApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 @Configuration
 @EnableAutoConfiguration
@@ -16,6 +18,13 @@ public class ArtifactServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ArtifactServiceApplication.class, args);
+	}
+
+	@Bean(name = "multipartResolver")
+	public CommonsMultipartResolver multipartResolver() {
+		CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+		multipartResolver.setMaxUploadSize(100000);
+		return multipartResolver;
 	}
 
 }
--- service/artifact-service/src/main/java/com/orness/gandalf/core/artifactservice/controller/ArtifactRestController.java
+++ service/artifact-service/src/main/java/com/orness/gandalf/core/artifactservice/controller/ArtifactRestController.java
@@ -96,16 +96,4 @@ public class ArtifactRestController {
                 .body(resource);
     }
 
-    @Bean
-    public MultipartConfigElement multipartConfigElement() {
-        return new MultipartConfigElement("");
-    }
-
-    @Bean
-    public MultipartResolver multipartResolver() {
-        org.springframework.web.multipart.commons.CommonsMultipartResolver multipartResolver = new org.springframework.web.multipart.commons.CommonsMultipartResolver();
-        multipartResolver.setMaxUploadSize(1000000);
-        return multipartResolver;
-    }
-
 }
