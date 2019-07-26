9514bb99969932b3ccc675f6e2d336ba070d4b16
Romain Fairant <romain.fairant@orness.com>
2019-07-26 13:44:16 +0200

    job module

---
+++ module/job-module/src/main/java/com/orness/gandalf/core/module/jobmodule/JobModuleApplication.java
@@ -0,0 +1,13 @@
+package com.orness.gandalf.core.module.jobmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobModuleApplication.class, args);
+	}
+
+}
