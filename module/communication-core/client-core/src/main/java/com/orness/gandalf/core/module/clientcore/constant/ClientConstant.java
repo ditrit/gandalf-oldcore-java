5318b81cc9a197cca26e18f2f5792879dddbaa61
Romain Fairant <romain.fairant@orness.com>
2019-09-19 16:57:27 +0200

    fix client properties cluster

--- module/communication-core/client-core/src/main/java/com/orness/gandalf/core/module/clientcore/constant/ClientConstant.java
+++ module/communication-core/client-core/src/main/java/com/orness/gandalf/core/module/clientcore/constant/ClientConstant.java
@@ -4,8 +4,8 @@ public class ClientConstant {
 
     public static final String COMMAND_CLIENT_SEND = "COMMAND_CLIENT_SEND";
 
-    public static final String GANDALF_CLUSTER_COMMAND_FRONTEND = "gandalf_cmd_frontend";
-    public static final String GANDALF_CLUSTER_EVENT_FRONTEND = "gandalf_event_frontend";
+    public static final String GANDALF_CLUSTER_COMMAND_FRONTEND = "gandalf-cmd-frontend";
+    public static final String GANDALF_CLUSTER_EVENT_FRONTEND = "gandalf-event-frontend";
     public static final String GANDALF_CLUSTER_ADDRESS = "Address";
     public static final String GANDALF_CLUSTER_PORT = "ServicePort";
 }
