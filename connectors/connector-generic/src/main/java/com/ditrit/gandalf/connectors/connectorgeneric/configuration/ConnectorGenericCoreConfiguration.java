638de7d2e4dab4289392fee1ea755394f062b5a6
Romain Fairant <romain.fairant@orness.com>
2019-10-14 15:31:36 +0200

    fix connectors generic cilent

--- connectors/connector-generic/src/main/java/com/ditrit/gandalf/connectors/connectorgeneric/configuration/ConnectorGenericCoreConfiguration.java
+++ connectors/connector-generic/src/main/java/com/ditrit/gandalf/connectors/connectorgeneric/configuration/ConnectorGenericCoreConfiguration.java
@@ -13,7 +13,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
-@ComponentScan(basePackages = {"com.ditrit.gandalf.core.connectorcore", "com.ditrit.gandalf.library.gandalfworkerclient", "com.ditrit.gandalf.modules.sourcecodeconnectors.sourcecodegandalf"})
+@ComponentScan(basePackages = {"com.ditrit.gandalf.core.connectorcore", "com.ditrit.gandalf.library.gandalfclient", "com.ditrit.gandalf.modules.sourcecodeconnectors.sourcecodegandalf"})
 @Order
 public class ConnectorGenericCoreConfiguration {
 
